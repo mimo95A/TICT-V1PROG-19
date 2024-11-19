20293313f634e0c674a68297cf1dd5e3de6e0bd9
Merry ashji <72072691+mimo95A@users.noreply.github.com>
2024-11-19 13:37:50 +0100

    Add files via upload

---
+++ 2_2.py
@@ -0,0 +1,3 @@
+list= eval(input('enter a list:'))
+x= max( list) - min( list)
+print(x)
---
+++ 2_3.py
@@ -0,0 +1,4 @@
+list=['A','C', 'B', 'B','C', 'A','C','C','B']
+list.sort()
+list1=[list.count('A'),list.count('B'),list.count('C')]
+print(list1)
